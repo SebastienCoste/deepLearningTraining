63068a24445796a0f07f33421074ef9e9989c725
Sebastien Coste <costseba@amazon.com>
2019-05-28 11:53:08 -0700

    optimise under par situation

--- src/diveintodl/puzzle/square.py
+++ src/diveintodl/puzzle/square.py
@@ -1,6 +1,5 @@
 from mxnet import nd
 import numpy as np
-import threading
 import multiprocessing
 import time
 
@@ -40,23 +39,48 @@ class Square:
             j2 = j + 1
         return i2, j2
 
+    def max_potential_remaining(self, weight):
+        length = len(weight)
+        max_potential = nd.zeros(shape=(length, length))
+        for i in reversed(range(length)):
+            for j in reversed(range(length)):
+                i_prev = i
+                j_prev = j+1
+                total_prev = 0
+                if j_prev == length:
+                    i_prev = i+1
+                    j_prev = 0
+                if i_prev < length:
+                    total_prev = max_potential[i_prev][j_prev]
+                max_potential[i][j] = 9 * weight[i][j] + total_prev
+        return max_potential
+
     def count_square(self, length, target_average):
         weight = self.matrix_path_per_step(length)
         print(weight)
         matrix_init = nd.ones(shape=(length, length))
         target_sum = target_average * weight[0][0]
+        max_remaining = self.max_potential_remaining(weight)
+        print(max_remaining)
         first = self.min
         while first <= self.max:
             matrix_init[0][0] = first
-            self.recursion_loop(weight, matrix_init, length, target_sum.asscalar(), 0, 1)
+            self.recursion_loop(weight, matrix_init, max_remaining, length, target_sum.asscalar(), 0, 1)
             first += 1
         return self.count
 
-    def recursion_loop(self, weight, matrix, length, target_sum, i, j):
+    def recursion_loop(self, weight, matrix, max_remaining, length, target_sum, i, j):
+
+        current_average = self.sum_all_paths(weight, matrix)
+
         if i < length:
             print('-'.join([''.join(['{:2}'.format(int(item)) for item in row]) for row in matrix.asnumpy()])
-                  + '|%d-%d|???/%d-%d|%d'
-                  % (i, j, target_sum, self.count, time.time() - self.start))
+                  + '|%d-%d|%d/%d-%d|%d'
+                  % (i, j, current_average, target_sum, self.count, time.time() - self.start))
+
+        if current_average + max_remaining[i][j] < target_sum:
+            return  # We cant make it, let's skip this and get higher number before
+
         for v in range(9):
             matrix[i][j] = v + 1
             current_average = self.sum_all_paths(weight, matrix)
@@ -70,7 +94,7 @@ class Square:
             elif current_average < target_sum:
                 i2, j2 = self.increment(i, j, length)
                 if i2 < length:
-                    self.recursion_loop(weight, matrix.copy(), length, target_sum, i2, j2)
+                    self.recursion_loop(weight, matrix.copy(), max_remaining, length, target_sum, i2, j2)
             else:
                 return
         return
@@ -108,6 +132,6 @@ class MultiSquare:
 
 
 MultiSquare(11, 110).run()
-# count = Square(0, 9, time.time()).count_square(2, 10)
+# count = Square(0, 9, time.time()).count_square(2, 20)
 # print("## TOTAL: %d" % count)
 
